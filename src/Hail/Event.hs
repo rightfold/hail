1f2b175336ee96d537980dcdee52ad06b03a5e09
rightfold <rightfold@gmail.com>
2017-04-30 20:36:32 +0200

    Add Event data type

---
+++ src/Hail/Event.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DeriveFunctor #-}
+module Hail.Event
+  ( Event(..)
+  , Source(..)
+  ) where
+
+import Data.ByteString (ByteString)
+import Data.Time.Clock (UTCTime)
+
+data Event a = Event
+  { eventTime :: UTCTime
+  , eventSource :: Source
+  , eventValue :: a
+  }
+  deriving (Show, Functor)
+
+data Source = Source
+  { sourceHost :: ByteString
+  , sourceService :: ByteString
+  }
+  deriving (Eq, Ord, Show)
